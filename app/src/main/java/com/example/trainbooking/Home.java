939e62dde021ee9a98564e5ca4207731d39f5462
foodycookie <onghao.howard@gmail.com>
2023-10-26 21:41:23 +0800

    Crocodile

---
+++ app/src/main/java/com/example/trainbooking/Home.java
@@ -0,0 +1,26 @@
+package com.example.trainbooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+        ImageButton imagebutton1 = (ImageButton) findViewById(R.id.imageButton2);
+        imagebutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, Login.class));
+            }
+        });
+    }
+}
