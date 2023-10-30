78e19bbc2943f79c0221538922e47e794d75cc43
foodycookie <onghao.howard@gmail.com>
2023-10-30 19:28:05 +0800

    Crocodile

--- app/src/main/java/com/example/trainbooking/Home.java
+++ app/src/main/java/com/example/trainbooking/Home.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Home extends AppCompatActivity {
@@ -13,7 +12,7 @@ public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+        setContentView(R.layout.activity_home);
 
         ImageButton imagebutton1 = (ImageButton) findViewById(R.id.imageButton2);
         imagebutton1.setOnClickListener(new View.OnClickListener() {
