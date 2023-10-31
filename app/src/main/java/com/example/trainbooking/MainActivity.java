5c4a55cc1d9bb86bc1d19f5aa926a3923fbef6d8
foodycookie <onghao.howard@gmail.com>
2023-11-01 00:37:15 +0800

    Crocodile

--- app/src/main/java/com/example/trainbooking/MainActivity.java
+++ app/src/main/java/com/example/trainbooking/MainActivity.java
@@ -2,13 +2,63 @@ package com.example.trainbooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton imageButtonHome1 = (ImageButton)findViewById(R.id.imageButtonHome1);
+
+        imageButtonHome1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, Info.class));
+            }
+        });
+
+        ImageButton imageButtonHome2 = (ImageButton)findViewById(R.id.imageButtonHome2);
+
+        imageButtonHome2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, Profile.class));
+            }
+        });
+
+        ImageButton imageButtonHome3 = (ImageButton)findViewById(R.id.imageButtonHome3);
+
+        imageButtonHome3.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
+        });
+
+        Button button1 = (Button)findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, SearchResult.class));
+            }
+        });
     }
 }
