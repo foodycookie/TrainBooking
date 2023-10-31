4e6ff33c3a1852f29e8bdf6f44f68ca55d9a164e
foodycookie <onghao.howard@gmail.com>
2023-11-01 00:17:50 +0800

    Crocodile

---
+++ app/src/main/java/com/example/trainbooking/TermsOfUse.java
@@ -0,0 +1,14 @@
+package com.example.trainbooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TermsOfUse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_of_use);
+    }
+}
---
+++ app/src/main/java/com/example/trainbooking/TrainStationDetail.java
@@ -0,0 +1,14 @@
+package com.example.trainbooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrainStationDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_train_station_detail);
+    }
+}
